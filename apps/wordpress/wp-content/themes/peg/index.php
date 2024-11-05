735e1810253656c5a319307abc7f05097c0368bc
Ian Kahare Gichero <37710249+iakah@users.noreply.github.com>
2024-11-05 13:26:48 +0300

    Update index.php

--- apps/wordpress/wp-content/themes/peg/index.php
+++ apps/wordpress/wp-content/themes/peg/index.php
@@ -1,12 +1,11 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <h1>TonyHe</h1>
-    <p>www.ouorz.com</p>
+    <h1>Ian Kahare</h1>
+    <p>Just A Rock Fan</p>
     <div class="contact">
-        <a class="github" href="https://github.com/ttttonyhe">Github: Helipengtony</a>
-        <a class="email" href="mailto:tony.hlp@hotmail.com">Email: tony.hlp@hotmail.com</a>
+        <a class="github" href="https://github.com/iakah">Github: iakah</a>
+        <a class="email" href="mailto:iakah2313@gmail.com">Email: iakah2313@gmail.com</a>
     </div>
 </div>
-
 <?php get_footer(); ?>
