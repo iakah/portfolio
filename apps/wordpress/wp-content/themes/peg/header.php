d020d13dd568e2b3e53138a9ffd62e007a02ec34
Ian Kahare Gichero <37710249+iakah@users.noreply.github.com>
2024-11-05 13:28:06 +0300

    Update header.php

--- apps/wordpress/wp-content/themes/peg/header.php
+++ apps/wordpress/wp-content/themes/peg/header.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TonyHe - Just A Poor Lifesinger</title>
-    <meta name="description" content="TonyHe's Blog | WordPress End">
+    <title>Ian Kahare - Just A Rock Fan</title>
+    <meta name="description" content="TIan's Blog | WordPress End">
     <meta name="robots" content="noindex, nofollow">
 </head>
 
